cdc9b2b5f1d8092d751af67b195931475a169859
chieger <charlie@codepath.com>
2017-04-04 12:27:35 -0700

    Initial Commit

---
+++ twitter_alamofire_demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Charles Hieger on 4/4/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
