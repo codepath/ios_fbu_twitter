167a82b58e14a14903c72a09c1385e6103847463
chieger <charlie@codepath.com>
2017-06-18 01:33:56 -0700

    Add User model

---
+++ twitter_alamofire_demo/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  twitter_alamofire_demo
+//
+//  Created by Charles Hieger on 6/17/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import Foundation
+
+class User {
+
+   //MARK: TODO: Add User Properties
+
+   //MARK: TODO: Add Initializer with dictionary
+
+   //MARK: TODO: Add currentUser Singleton and get / set
+}
