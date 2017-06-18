548b85aca308dbe526835033fe4a6007c61e1813
chieger <charlie@codepath.com>
2017-06-18 13:36:08 -0700

    Add TimelineViewController and Tweet Class

---
+++ twitter_alamofire_demo/TimelineViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TimelineViewController.swift
+//  twitter_alamofire_demo
+//
+//  Created by Charles Hieger on 6/18/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class TimelineViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ twitter_alamofire_demo/Tweet.swift
@@ -0,0 +1,9 @@
+//
+//  Tweet.swift
+//  twitter_alamofire_demo
+//
+//  Created by Charles Hieger on 6/18/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import Foundation
