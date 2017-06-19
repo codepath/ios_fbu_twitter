9fb413777273883ecdd6f9d803cadfef63ef9952
chieger <charlie@codepath.com>
2017-06-19 02:23:30 -0700

    Add Tweet Cell

---
+++ twitter_alamofire_demo/TweetCell.swift
@@ -0,0 +1,24 @@
+//
+//  TweetCell.swift
+//  twitter_alamofire_demo
+//
+//  Created by Charles Hieger on 6/18/17.
+//  Copyright © 2017 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
