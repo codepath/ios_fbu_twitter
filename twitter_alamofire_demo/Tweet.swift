f5162fd02967866d1e65df5d0a00c2aba1163198
chieger <charlie@codepath.com>
2017-06-19 02:23:56 -0700

    Add Tweet and User classes

--- twitter_alamofire_demo/Tweet.swift
+++ twitter_alamofire_demo/Tweet.swift
@@ -7,3 +7,51 @@
 //
 
 import Foundation
+
+class Tweet {
+
+   //MARK: Properties
+   var id: Int? // For favoriting, retweeting & replying
+   var user: User? // Contains name, screenname, etc. of tweet author
+   var createdAtString: String? // Display date
+   var text: String? // Text content of tweet
+   var favoriteCount: Int? // Update favorite count label
+   var favorited: Bool? // Configure favorite button
+   var retweetCount: Int? // Update favorite count label
+   var retweeted: Bool? // Configure retweet button
+   var profileImageURL: URL? // For fetching profile image
+
+
+   //MARK: Add regular init
+
+   //MARK: Add init with dictionary
+   init(dictionary: [String: Any]) {
+      id = dictionary["id"] as? Int
+      if let user = dictionary["user"] as? [String: Any] {
+         self.user = User(dictionary: user)
+      }
+      // Format createdAt Date
+      // Get the original date string
+      if let originalCreatedAtString = dictionary["created_at"] as? String {
+         let formatter = DateFormatter()
+         // Configure the format to parse the original date string
+         formatter.dateFormat = "EEE MMM d HH:mm:ss Z y"
+         // Convert String to Date
+         if let createdAtDate = formatter.date(from: originalCreatedAtString) {
+            // Configure output format
+            formatter.dateStyle = .short
+            formatter.timeStyle = .none
+            // Convert Date to String
+            createdAtString = formatter.string(from: createdAtDate)
+         }
+      }
+      text = dictionary["text"] as? String
+      favoriteCount = dictionary["favorite_count"] as? Int
+      favorited = dictionary["favorited"] as? Bool
+      retweetCount = dictionary["retweet_count"] as? Int
+      retweeted = dictionary["retweeted"] as? Bool
+      if let profileImageString = dictionary["profile_image_url_https"] as? String {
+         profileImageURL = URL(string: profileImageString)
+      }
+   }
+}
--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -11,10 +11,43 @@ import Foundation
 class User {
 
    //MARK: TODO: Add User Properties
+   var name: String?
+   var screenName: String?
+
+   // For user persistance
+   var dictionary: [String: Any]?
 
    //MARK: TODO: Add Initializer with dictionary
+   init(dictionary: [String: Any]) {
+      name = dictionary["name"] as? String
+      screenName = dictionary["screen_name"] as? String
+      self.dictionary = dictionary
+   }
 
    //MARK: TODO: Add current user singleton
-
+   private static var _currentUser: User?
    //MARK: TODO: Persist user
+   static var currentUser: User? {
+      get {
+         if _currentUser == nil {
+            let defaults = UserDefaults.standard
+            if let userData = defaults.data(forKey: "currentUserData") {
+               let dictionary = try! JSONSerialization.jsonObject(with: userData, options: []) as! [String: Any]
+               _currentUser = User(dictionary: dictionary)
+            }
+         }
+         return _currentUser
+      }
+
+      set (user) {
+         _currentUser = user
+         let defaults = UserDefaults.standard
+         if let user = user {
+            let data = try! JSONSerialization.data(withJSONObject: user.dictionary!, options: [])
+            defaults.set(data, forKey: "currentUserData")
+         } else {
+            defaults.removeObject(forKey: "currentUserData")
+         }
+      }
+   }
 }
