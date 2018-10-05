189b201e4bedc4f7cc0bf3838ecb43d1100edee5
hlpostman <users.noreply.github.com>
2018-10-05 17:03:32 -0400

    Add basic Tweet class

---
+++ twitter_alamofire_demo/Tweet.swift
@@ -0,0 +1,15 @@
+//
+//  Tweet.swift
+//  twitter_alamofire_demo
+//
+//  Created by Aristotle on 2018-10-05.
+//  Copyright © 2018 Charles Hieger. All rights reserved.
+//
+
+import UIKit
+
+class Tweet: NSObject {
+    init(dictionary: [String : Any]) {
+        super.init()
+    }
+}
