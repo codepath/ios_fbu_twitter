15b10fe77de41cda03b314a2239887d9279fa9f7
hlpostman <users.noreply.github.com>
2018-10-12 16:01:03 -0400

    Reconnect tableview outlet to TimelineViewController

--- twitter_alamofire_demo/TimelineViewController.swift
+++ twitter_alamofire_demo/TimelineViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TimelineViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
